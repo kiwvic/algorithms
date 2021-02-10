d42472006c6cc4f786b692d09ea821b4e71ba2c7
PC <you@example.com>
2021-02-10 21:44:28 +0200

    Added bubble sort + improved b. sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,18 +1,20 @@
 def bubble_sort(lst):
-	for i in range(len(lst) - 1):
-		for j in range(len(lst) - 1):
-			if lst[j] > lst[j + 1]:
-				lst[j], lst[j + 1] = lst[j + 1], lst[j]
-	return lst
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    return lst
 
 
 def improved_bubble_sort(lst):
-	i = 0
-	bl = True
-	while bl:
-		for i in range(len(lst) - 1):
-			bl = False
-			if lst[j] > lst[j + 1]:
-				lst[j], lst[j + 1] = lst[j + 1], lst[j]
-				bl = True
-	return lst
+    i = 0
+    bl = True
+    while bl:
+        bl = False
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                bl = True
+        i += 1
+    return lst
+
